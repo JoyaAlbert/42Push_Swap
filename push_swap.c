dd195486da05212fb61477ee357d7c6da3bd4308
JoyaAlbert <albertojoyapincu2@gmail.com>
2024-08-05 22:43:24 +0200

    a

--- push_swap.c
+++ push_swap.c
@@ -45,8 +45,6 @@ void	sort_stack(t_stacks **stack_a, t_stacks **stack_b, t_data *nab)
 	int	j;
 	int	stack_size;
 
-	//if (!stack_a || !*stack_a || !(*stack_a)->next)
-	//	return ;
 	i = 0;
 	stack_size = ft_lstsize(*stack_a);
 	while (is_sorted(stack_a) == -1)
@@ -65,80 +63,6 @@ void	sort_stack(t_stacks **stack_a, t_stacks **stack_b, t_data *nab)
 			pa(stack_a, stack_b, nab);
 	}
 }
-// 1084
-/**    
-void	sort_stack(t_stacks **stack_a, t_stacks **stack_b, t_data *nab)
-{
-	int		i;
-	int		j;
-	int		size;
-
-	i = 0;
-	size = ft_lstsize(*stack_a);
-	while (is_sorted(stack_a) != 0)
-	{
-		j = 0;
-		while (j++ < size)
-		{
-			if ((((*stack_a)->index >> i) & 1) == 1)
-				ra(stack_a);
-			else
-				pb(stack_b, stack_a, nab);
-		}
-		while (ft_lstsize(*stack_b) != 0)
-			pa(stack_a, stack_b, nab);
-		i++;
-	}
-}*/
-//1084
-/**
-static int	get_max_bits(t_stacks **stack)
-{
-	t_stacks	*head;
-	int		max;
-	int		max_bits;
-
-	head = *stack;
-	max = head->index;
-	max_bits = 0;
-	while (head)
-	{
-		if (head->index > max)
-			max = head->index;
-		head = head->next;
-	}
-	while ((max >> max_bits) != 0)
-		max_bits++;
-	return (max_bits);
-}
-void	sort_stack(t_stacks **stack_a, t_stacks **stack_b, t_data *nab)
-{
-		t_stacks	*head_a;
-	int		i;
-	int		j;
-	int		size;
-	int		max_bits;
-
-	i = 0;
-	head_a = *stack_a;
-	size = ft_lstsize(head_a);
-	max_bits = get_max_bits(stack_a);
-	while (i < max_bits)
-	{
-		j = 0;
-		while (j++ < size)
-		{
-			head_a = *stack_a;
-			if (((head_a->index >> i) & 1) == 1)
-				ra(stack_a);
-			else
-				pb(stack_b, stack_a, nab);
-		}
-		while (ft_lstsize(*stack_b) != 0)
-			pa(stack_a, stack_b, nab);
-		i++;
-	}
-}  */
 
 int	main(int argc, char **argv)
 {
